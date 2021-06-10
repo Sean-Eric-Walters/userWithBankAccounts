faf4024837ebf1f6e079b6048e9ca2222c3a3158
Sean-Eric-Walters <seanericwalters@gmail.com>
2021-06-10 12:21:43 -0700

    ch1

--- awba.py
+++ awba.py
@@ -27,42 +27,44 @@ class BankAccount:
         return self
 
         
-class user:
+class User:
     def __init__(self, name, email):
         self.name = name
         self.email = email
         self.account = BankAccount(int_rate=0.02, balance=0)
 
     def make_deposit(self, amount):
-        self.account_balance.make_deposit(amount)
+        self.account.deposit(amount)
         return self
 
     def make_withdrawal(self, amount):
-        self.account_balance.make_withdrawal(amount)
+        self.account.withdrawal(amount)
         return self
     
-    def dispaly_user_balance(self):
-        print(self.name, self.isplay_account_info)
+    def display_user_balance(self):
+        print(f"{self.name}, Balance: ${self.account.balance}")
 
-    def tranfer_money(self, other_user, amount):
-        self.account_balance -= amount
-        other_user.account_balance += amount
-        return self
+    def transfer_money(self, other_user, amount):
+        self.account.balance += amount
+        other_user.account.balance -= amount
 
-Batou = user('Batou', 'Batou@section9.gov' )
-Togusa = user('Togusa', 'Togusa@section9.gov')
-Major = user('Major', 'Major@section9.gov')
 
-print('==balance==')
 
-Batou.make_deposit(4999).make_deposit(4999).make_deposit(1698.58).make_withdrawal(2800).dispaly_user_balance()
+Batou = User('Batou', 'Batou@section9.gov' )
+Togusa = User('Togusa', 'Togusa@section9.gov')
+Major = User('Major', 'Major@section9.gov')
 
-Togusa.make_deposit(1589.31).make_withdrawal(243.19).make_deposit(1589.31).make_withdrawal(2000).dispaly_user_balance()
 
-Major.make_deposit(9000).make_withdrawal(1100).make_withdrawal(89.13).make_withdrawal(983.21).dispaly_user_balance()
 
-print('==balance==')
+Batou = User('Batou', 'Batou@section9.gov' )
+Togusa = User('Togusa', 'Togusa@section9.gov')
+Major = User('Major', 'Major@section9.gov')
 
-Batou.tranfer_money(Major,3000)
-Batou.dispaly_user_balance()
-Major.dispaly_user_balance()
+
+
+Batou.make_deposit(4999).make_deposit(4999).make_deposit(1698.58).make_withdrawal(2800).display_user_balance()
+
+Togusa.make_deposit(1589.31).make_withdrawal(243.19).make_deposit(1589.31).make_withdrawal(2000).display_user_balance()
+
+Major.make_deposit(9000).make_withdrawal(1100).make_withdrawal(89.13).make_withdrawal(983.21).display_user_balance()
+
